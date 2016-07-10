0593c02c2872540dcc35cebdba529956f5c0162c
kon <idontwanttogoonthecart@gmail.com>
2016-07-10 20:35:26 +0300

    creating Notes component

---
+++ src/components/Notes/Notes.js
@@ -0,0 +1,23 @@
+import React, {PropTypes} from 'react';
+import './styles.scss';
+
+const Notes = ({todoList, mode, ...handlers}) => {
+
+  let list = todoList
+    .filter((item) => {
+     //filter notes with unwanted statuses
+    })
+    .map((item, key) => {
+      return (
+        <div>item.title</div>
+      );
+    });
+
+
+  return (
+    <div className="notes">{list}</div>
+  );
+
+};
+
+export default Notes;
