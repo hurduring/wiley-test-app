8e3d593049aeecb3c7a99d5c57acbbf9367e1981
kon <idontwanttogoonthecart@gmail.com>
2016-07-10 20:26:53 +0300

    setting up handlers for TodoInput component

--- src/containers/Home/handlers.js
+++ src/containers/Home/handlers.js
@@ -1,5 +1,23 @@
+import uuid from 'uuid';
+import {
+  ENTER_KEY
+} from '../../constants';
+
 export const addNewNote = function (e) {
+  if (e.keyCode !== ENTER_KEY) {
+    return;
+  }
+
+  this.setState({
+    ...this.state,
+    newTodo: '',
+    todoList: [
+      ...this.state.todoList,
+      {id: uuid.v1(), title: e.target.value.trim(), status: STATUS.ACTIVE}
+    ]
+  });
 };
 
 export const updateNewNote = function (e) {
+  this.setState({...this.state, newTodo: e.target.value});
 };
