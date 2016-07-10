915e0d67fa441c13a24da69d8b234bc9227d6be1
kon <idontwanttogoonthecart@gmail.com>
2016-07-10 21:51:11 +0300

    wiring handlers and Note component

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,6 +1,14 @@
 import React, {PropTypes} from 'react';
 import classNames from 'classnames';
+import ReactDOM from 'react-dom';
 import './styles.scss';
+
+import {
+  makeEditable,
+  deleteNote,
+  updateNoteTitle,
+  updateNoteStatus
+} from './handlers';
 
 class Note extends React.Component {
   constructor(props) {
@@ -9,6 +17,7 @@ class Note extends React.Component {
     this.state = {
       editing: false
     };
+
   }
 
   render() {
@@ -20,42 +29,50 @@ class Note extends React.Component {
             id={`complete-${this.props.id}`}
             type="checkbox"
             checked={this.props.status}
+            onChange={updateNoteStatus.bind(this)}
           />
           <label htmlFor={`complete-${this.props.id}`}>
-            <i
-              className={
-                  classNames({
-                    "fa fa-circle-o": !this.props.status,
-                    "fa fa-check-circle-o": this.props.status
-                  })
-                }
-            >
+            <i className={classNames({
+                "fa fa-circle-o": !this.props.status,
+                "fa fa-check-circle-o": this.props.status
+                })}>
             </i>
           </label>
         </div>
 
-        <div className={classNames({hidden: this.state.editing, "note__title": true})}>
+        <div
+          className={classNames({hidden: this.state.editing, "note__title": true})}
+        >
           {this.props.title}
         </div>
 
-        <div className={classNames({hidden: this.state.editing, "note__edit-button": true})}>
+        <div
+          className={classNames({hidden: this.state.editing, "note__edit-button": true})}
+          onClick={makeEditable.bind(this)}
+        >
           <i className="fa fa-edit"></i>
         </div>
 
-        <div className={classNames({hidden: this.state.editing, "note__delete-button": true})}>
+        <div
+          className={classNames({hidden: this.state.editing, "note__delete-button": true})}
+          onClick={deleteNote.bind(this)}
+        >
           <i className="fa fa-remove"></i>
         </div>
 
         <input
+          ref="editField"
           className={classNames({visible: this.state.editing, "note__input": true})}
           type="text"
           value={this.props.title}
+          onKeyDown={updateNoteTitle.bind(this)}
+          onBlur={updateNoteTitle.bind(this)}
+          onChange={updateNoteTitle.bind(this)}
         />
 
       </div>
     );
   }
 }
-
 
 export default Note;
